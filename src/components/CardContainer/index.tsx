1e2c120268918cb0fe3a203124cdca0dff17a0d8
Ali Kaner <ali.kaner@hotmail.com>
2023-08-08 13:33:18 +0300

    fix: map key

--- src/components/CardContainer/index.tsx
+++ src/components/CardContainer/index.tsx
@@ -5,7 +5,7 @@ export function CardContainer({ cards }: { cards: CharacterType[] }) {
   return (
     <div className="min-h-screen w-screen flex flex-wrap flex-shrink-0 gap-6 bg-secondary p-4 items-center justify-center">
       {cards.map((card) => (
-        <Card cardProps={card}/>
+        <Card key={card.id} cardProps={card}/>
       ))}
     </div>
   );
